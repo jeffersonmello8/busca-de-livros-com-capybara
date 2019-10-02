eb5ecd074896bcd31c7e977f5c9848a4e3e5b98c
jefferson <jefferson.mello8@hotmail.com>
2019-10-02 08:47:33 -0300

    Melhorando identação do código

--- spec/buscadelivros_spec.rb
+++ spec/buscadelivros_spec.rb
@@ -1,57 +1,55 @@
-describe 'Buscando livros na casa do código', :buscanacasadocodigo do
-    
-    def preencherlivro (livro) 
-        fill_in 'campoBuscaPrincipal', with: livro
-    end 
+describe "Buscando livros na casa do código", :buscanacasadocodigo do
+  def preencherlivro(livro)
+    fill_in "campoBuscaPrincipal", with: livro
+  end
 
-    def clicarnolivro (titulo)
-        find('.busca-lista .busca-item .busca-item-titulo', text: titulo).click
-    end 
+  def clicarnolivro(titulo)
+    find(".busca-lista .busca-item .busca-item-titulo", text: titulo).click
+  end
 
-    def verificarnomedolivro(nome)
-        res = find('.cabecalhoProdutoLivro-titulo-principal')
-        expect(res).to have_content nome
-    end 
+  def verificarnomedolivro(nome)
+    res = find(".cabecalhoProdutoLivro-titulo-principal")
+    expect(res).to have_content nome
+  end
 
-    def verificarsubtitulo(subtitulo)
-        res = find('.cabecalhoProdutoLivro-titulo-sub')
-        expect(res).to have_content subtitulo
-    end 
+  def verificarsubtitulo(subtitulo)
+    res = find(".cabecalhoProdutoLivro-titulo-sub")
+    expect(res).to have_content subtitulo
+  end
 
-    def verificarautor(autor)
-        res = find('.cabecalhoProdutoLivro-nomeAutor')
-        expect(res).to have_content autor
-    end 
+  def verificarautor(autor)
+    res = find(".cabecalhoProdutoLivro-nomeAutor")
+    expect(res).to have_content autor
+  end
 
-    before(:each) do 
-        visit '/'
-    end 
+  before(:each) do
+    visit "/"
+  end
 
-    it 'Buscando livro de Rspec', :livrorspec do
-        preencherlivro('rspec')
-        click_button 'buscar'
-        clicarnolivro('RSpec: Crie especificações executáveis em Ruby')
-        verificarnomedolivro('RSpec')
-        verificarsubtitulo('Crie especificações executáveis em Ruby')
-        verificarautor('Mauro George')     
-    end 
+  it "Buscando livro de Rspec", :livrorspec do
+    preencherlivro("rspec")
+    click_button "buscar"
+    clicarnolivro("RSpec: Crie especificações executáveis em Ruby")
+    verificarnomedolivro("RSpec")
+    verificarsubtitulo("Crie especificações executáveis em Ruby")
+    verificarautor("Mauro George")
+  end
 
-    it 'Buscando livro de Cucumber', :livrocucumber do        
-        preencherlivro('cucumber')
-        click_button 'buscar'
-        clicarnolivro('Cucumber e RSpec: Construa aplicações Ruby com testes e especificações')
-        verificarnomedolivro('Cucumber e RSpec')
-        verificarsubtitulo('Construa aplicações Ruby com testes e especificações')
-        verificarautor('Hugo Baraúna')   
-    end 
-     
-    it 'Buscando livro de Selenium WebDriver', :livroselenium do
-        preencherlivro('selenium')
-        click_button 'buscar'
-        clicarnolivro('Selenium WebDriver: Descomplicando testes automatizados com Java')
-        verificarnomedolivro('Selenium WebDriver')
-        verificarsubtitulo('Descomplicando testes automatizados com Java')
-        verificarautor('Rafael Peixoto')
-    end 
+  it "Buscando livro de Cucumber", :livrocucumber do
+    preencherlivro("cucumber")
+    click_button "buscar"
+    clicarnolivro("Cucumber e RSpec: Construa aplicações Ruby com testes e especificações")
+    verificarnomedolivro("Cucumber e RSpec")
+    verificarsubtitulo("Construa aplicações Ruby com testes e especificações")
+    verificarautor("Hugo Baraúna")
+  end
 
+  it "Buscando livro de Selenium WebDriver", :livroselenium do
+    preencherlivro("selenium")
+    click_button "buscar"
+    clicarnolivro("Selenium WebDriver: Descomplicando testes automatizados com Java")
+    verificarnomedolivro("Selenium WebDriver")
+    verificarsubtitulo("Descomplicando testes automatizados com Java")
+    verificarautor("Rafael Peixoto")
+  end
 end
